66006279d9df514db94711b9242d69e635788442
temps <zrot777@gmail.com>
2021-01-19 12:38:20 +0300

    1

--- src/main/java/TelegramBot/Application.java
+++ src/main/java/TelegramBot/Application.java
@@ -18,13 +18,13 @@ public class Application {
         telegram_bot.botConnect();
 
 
-        MessageReciever messageReciever=new MessageReciever(telegram_bot);
+        /*MessageReciever messageReciever=new MessageReciever(telegram_bot);
         Thread threadRecievr=new Thread(messageReciever);
         threadRecievr.start();
 
         MessageSender messageSender=new MessageSender(telegram_bot);
         Thread threadSender=new Thread(messageSender);
-        threadSender.start();
+        threadSender.start();*/
 
 
 
